3f9ae1ded01c5cb2565293f090d67453cd209b74
TruongBach <truongcongbach1996@gmail.com>
2023-07-15 08:27:34 +0700

    clean home page

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,22 +1,10 @@
 import { BasicLayout } from '../layout'
-import { useLogin, useLogout } from '../../resource-api/hooks/auth'
 import React from 'react'
 
 function Home() {
-  const login = useLogin();
-  const logout = useLogout();
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
   return <div>Home page
     <br/>
-    <input type='text' value={email} onChange={e => setEmail(e.target.value)}/>
-    <input type='text' value={password} onChange={e => setPassword(e.target.value)}/>
 
-    <button onClick={() => login({
-      email,
-      password,
-    })}>Login</button>
-    <button onClick={() => logout()}>logout</button>
   </div>
 }
 Home.Layout = BasicLayout;
