61bf41f6821c14c9af603bc03b513173f4c0b640
TruongBach <truongcongbach1996@gmail.com>
2023-07-15 08:54:20 +0700

    remove vercel.json config

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -2,9 +2,9 @@ import { BasicLayout } from '../layout'
 import React from 'react'
 
 function Home() {
-  return <div>Home page2
+  return <div>
     <br/>
-
+    Home page1111
   </div>
 }
 Home.Layout = BasicLayout;
