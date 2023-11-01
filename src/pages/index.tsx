6b053662ed58d35f8f64951b6e658206486aa2e8
TruongBach <truongcongbach1996@gmail.com>
2023-11-02 00:20:30 +0700

    config build with cache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
 import { Home } from '../components/home'
-import { promises as fs } from 'fs'
-import path from 'path'
+// import { promises as fs } from 'fs'
+// import path from 'path'
 
 export async function getStaticProps() {
-  fs.writeFile(
-    path.join(process.cwd(), 'public','locales',`common.json`),
-    JSON.stringify({
-      "description": "description test bach"
-    })
-  ).then(res => {
-    console.log('index '+ 1, res)
-  }).catch(error => {
-    console.log('index '+ 2, error)
-  })
+  // fs.writeFile(
+  //   path.join(process.cwd(), 'public','locales',`common.json`),
+  //   JSON.stringify({
+  //     "description": "description test bach"
+  //   })
+  // ).then(res => {
+  //   console.log('index '+ 1, res)
+  // }).catch(error => {
+  //   console.log('index '+ 2, error)
+  // })
   return { props: {} }
 }
 
