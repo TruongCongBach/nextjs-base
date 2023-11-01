97af20ad1c4570d55cf86e758aa49b633f8837d7
TruongBach <truongcongbach1996@gmail.com>
2023-11-02 00:16:36 +0700

    config build with cache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export async function getStaticProps() {
   fs.writeFile(
     path.join(process.cwd(), 'public','locales',`common.json`),
     JSON.stringify({
-      "description": "description test"
+      "description": "description test bach"
     })
   ).then(res => {
     console.log('index '+ 1, res)
