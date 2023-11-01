24a0cd35ea69fd608dd8047c635434dabf0f14b8
TruongBach <truongcongbach1996@gmail.com>
2023-11-02 00:23:02 +0700

    config build with cache

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,23 +3,23 @@ import { promises as fs } from 'fs'
 import path from 'path'
 
 export async function getServerSideProps() {
-  fs.readFile(path.join(process.cwd(), 'public', 'locales', 'de', `common.json`)).then(res => {
-    console.log(1, res)
+  fs.readFile(path.join(process.cwd(), 'public', 'locales', 'de', `common.json`)).then((res:any) => {
+    console.log(1, JSON.parse(res))
   }).catch(error => {
     console.log(2, error)
   })
 
-  fs.readFile(path.join('public', 'locales', 'de', `common.json`)).then(res => {
-    console.log(3, res)
+  fs.readFile(path.join('public', 'locales', 'de', `common.json`)).then((res:any) => {
+    console.log(3, JSON.parse(res))
   }).catch(error => {
     console.log(4, error)
   })
 
-  fs.readFile(path.join('public', 'locales', `common.json`)).then((res:any) => {
-    console.log(4, JSON.parse(res))
-  }).catch(error => {
-    console.log(5, error)
-  })
+  // fs.readFile(path.join('public', 'locales', `common.json`)).then((res:any) => {
+  //   console.log(4, JSON.parse(res))
+  // }).catch(error => {
+  //   console.log(5, error)
+  // })
   return { props: {} }
 }
 
