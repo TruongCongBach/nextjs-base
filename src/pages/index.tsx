bbb75478f204d8853397ca54a92ad03ab9be772b
TruongBach <truongcongbach1996@gmail.com>
2023-11-01 23:47:30 +0700

    config build with cache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
 import { Home } from '../components/home'
+import { promises as fs } from 'fs'
+import path from 'path'
 
-export async function getServerSideProps() {
-  // Fetch data from external API
-  // const res = await fetch(`https://.../data`)
-  // const data = await res.json()
+export async function getStaticProps() {
+  fs.readFile(path.join(process.cwd(), 'public', 'locales', 'de', `common.json`)).then(res => {
+    console.log(1, res)
+  }).catch(error => {
+    console.log(2, error)
+  })
 
-  // Pass data to the page via props
-  return { props: { } }
+  fs.readFile(path.join('public', 'locales', 'de', `common.json`)).then(res => {
+    console.log(3, res)
+  }).catch(error => {
+    console.log(4, error)
+  })
+  return { props: {} }
 }
+
 export default Home
