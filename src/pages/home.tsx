04f99396d522924e1e5d06b80d697198b389a65b
TruongBach <truongcongbach1996@gmail.com>
2023-11-02 00:33:39 +0700

    config build with cache

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,25 @@
 import { Home } from '../components/home'
-import { promises as fs } from 'fs'
-import path from 'path'
+// import { promises as fs } from 'fs'
+// import path from 'path'
 
 export async function getServerSideProps() {
-  fs.readFile(path.join(process.cwd(), 'public', 'locales', 'de', `common.json`)).then((res:any) => {
-    console.log(1, JSON.parse(res))
-  }).catch(error => {
-    console.log(2, error)
-  })
-
-  fs.readFile(path.join('public', 'locales', 'nl', `common.json`)).then((res:any) => {
-    console.log(3, JSON.parse(res))
-  }).catch(error => {
-    console.log(4, error)
-  })
-
-  fs.readFile(path.join('public', 'locales','nl', `common.json`)).then((res:any) => {
-    console.log(4, JSON.parse(res))
-  }).catch(error => {
-    console.log(5, error)
-  })
+  // fs.readFile(path.join(process.cwd(), 'public', 'locales', 'de', `common.json`)).then((res:any) => {
+  //   console.log(1, JSON.parse(res))
+  // }).catch(error => {
+  //   console.log(2, error)
+  // })
+  //
+  // fs.readFile(path.join('public', 'locales', 'nl', `common.json`)).then((res:any) => {
+  //   console.log(3, JSON.parse(res))
+  // }).catch(error => {
+  //   console.log(4, error)
+  // })
+  //
+  // fs.readFile(path.join('public', 'locales','nl', `common.json`)).then((res:any) => {
+  //   console.log(4, JSON.parse(res))
+  // }).catch(error => {
+  //   console.log(5, error)
+  // })
   return { props: {} }
 }
 
