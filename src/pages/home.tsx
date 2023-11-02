455d00b497aa715aa7ce00eb259775f476e74d81
TruongBach <truongcongbach1996@gmail.com>
2023-11-02 15:19:00 +0700

    ˆ

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,19 +3,19 @@ import { promises as fs } from 'fs'
 import path from 'path'
 
 export async function getServerSideProps() {
-  fs.readFile(path.join(process.cwd(), 'public', 'locales', 'de', `common.json`)).then((res:any) => {
+  await fs.readFile(path.join(process.cwd(), 'public', 'locales', 'de', `common.json`)).then((res:any) => {
     console.log(1, JSON.parse(res))
   }).catch(error => {
     console.log(2, error)
   })
   console.log('de', path.join(process.cwd()))
-  fs.readFile(path.join('public', 'locales', 'de', `common.json`)).then((res:any) => {
+  await fs.readFile(path.join('public', 'locales', 'de', `common.json`)).then((res:any) => {
     console.log(3, JSON.parse(res))
   }).catch(error => {
     console.log(4, error)
   })
   console.log('nl', path.join(process.cwd()))
-  fs.readFile(path.join('public', 'locales','nl', `common.json`)).then((res:any) => {
+  await fs.readFile(path.join('public', 'locales','nl', `common.json`)).then((res:any) => {
     console.log(4, JSON.parse(res))
   }).catch(error => {
     console.log(5, error)
